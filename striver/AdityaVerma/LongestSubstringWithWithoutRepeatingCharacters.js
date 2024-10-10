adbb72c7270935adbed1d25fc5bedf7fe1f1a2b7
Priyanka Katre <priyanka.katre@greenmangaming.com>
2024-10-10 11:49:45 +0100

    Longest Substring With Without Repeating Characters

--- striver/AdityaVerma/LongestSubstringWithWithoutRepeatingCharacters.js
+++ striver/AdityaVerma/LongestSubstringWithWithoutRepeatingCharacters.js
@@ -3,10 +3,21 @@ const LongestSubstringWithWithoutRepeatingCharacters = (arr, k) => {
     while (right < arr.length) {
         if (!set.has(arr[right])) {
             set.add(arr[right])
+            if (set.size === k) {
+                result = Math.max(result, right-left+1)
+            }
+            right++;
+        } else {
+            set.delete(arr[left]);
+            left++
         }
-        right++
+        if (right - left + 1 > k) {
+            set.delete(arr[left])
+            left++
+        }
+
     }
-    return set
+    return result
 
 }
 console.log(LongestSubstringWithWithoutRepeatingCharacters('pwwkew', 3));
