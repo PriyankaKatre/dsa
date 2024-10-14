3d4c2b33982eed1ae0e13d4c43e17c7e22c8e5ad
Priyanka Katre <priyankack1707@gmail.com>
2024-10-14 16:15:25 +0100

    Stack NEAREST SMALLER TO LEFT

---
+++ striver/AdityaVerma/stack/NEARESTSMALLERTOLEFT.js
@@ -0,0 +1,27 @@
+const NEARESTSMALLERTOLEFT = (arr) => {
+    let stack = [];
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (stack.length === 0) {
+            result.push(-1)
+        }
+        else if (stack.length > 0 && stack[stack.length - 1] < arr[i]) {
+            result.push(stack[stack.length - 1]);
+        }
+        else if (stack.length > 0 && stack[stack.length - 1] >= arr[i]) {
+            while (stack.length > 0 && stack[stack.length - 1] >= arr[i]) {
+                stack.pop()
+            }
+            if (stack.length === 0) {
+                result.push(-1)
+            }
+            else {
+                result.push(stack[stack.length - 1]);
+            }
+        }
+
+        stack.push(arr[i])
+    }
+    return result
+}
+console.log(NEARESTSMALLERTOLEFT([4, 5, 2, 10, 8]));
