644772c029bd67dbb023b6bbf77631d524d30a5a
Priyanka Katre <priyankack1707@gmail.com>
2024-10-18 10:41:37 +0100

    Number printing based on row

--- striver/Patterns/trianglePatternNumbers.js
+++ striver/Patterns/trianglePatternNumbers.js
@@ -2,9 +2,20 @@ const trianglePatternNumbers = (n) => {
     for (let i = 0; i < n; i++) {
         let row= ''
         for (let j = 0; j <= i; j++) {
-            row+=j+1
+            row+=i+1
         }
         console.log(row)
     }
 }
 trianglePatternNumbers(5);
+
+// const trianglePatternNumbers = (n) => {
+//   for (let i = 0; i < n; i++) {
+//     let row = "";
+//     for (let j = 0; j <= i; j++) {
+//       row += j + 1;
+//     }
+//     console.log(row);
+//   }
+// };
+// trianglePatternNumbers(5);
