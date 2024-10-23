e7e45e3bcaed31c918be9e5d520caa2048687807
Priyanka Katre <priyankack1707@gmail.com>
2024-10-23 11:17:48 +0100

    rght Angle Triangle WithGap Between

---
+++ striver/Patterns/rghtAngleTriangleWithGapBetween.js
@@ -0,0 +1,24 @@
+const binaryRightAngleTriangle = (n) => {
+    for (let i = 0; i < n; i++) {
+        let row1 = '';
+        let row2 = "";
+        let row3 = "";
+
+        let space = 2*(n-i-1)
+        for (let j = 0; j <= i; j++) {
+            row1+=j+1
+        }
+        for (let j = 0; j < space ; j++) {
+            row3 += " ";
+        }
+
+        for (let k = i; k >= 0; k--) {
+          row2 += k + 1;
+        }
+        //
+        console.log(row1,row3,row2)
+        //console.log(row2);
+
+    }
+}
+binaryRightAngleTriangle(4)
