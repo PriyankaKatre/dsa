774a96cae69f5633f0be4e29e6a8a1e432a31baa
Priyanka Katre <priyankack1707@gmail.com>
2024-10-23 13:56:57 +0100

    right Angle Triangle increment Alphabets

---
+++ striver/Patterns/rightAngleTriangleincrementAlphabets.js
@@ -0,0 +1,11 @@
+const rightAngleTriangleincrementAlphabets = (n) => {
+  for (let i = 0; i <= n; i++) {
+    let row = "";
+    for (let j = 0; j < i; j++) {
+      row += String.fromCharCode("A".charCodeAt(0) + j);
+    }
+    console.log(row);
+  }
+};
+
+rightAngleTriangleincrementAlphabets(5);
