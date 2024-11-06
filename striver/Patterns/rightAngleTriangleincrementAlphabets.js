d6e4d206fe9848ead9189bee0462f73027fc6797
Priyanka Katre <priyankack1707@gmail.com>
2024-11-06 10:11:29 +0000

    comparasion sort

--- striver/Patterns/rightAngleTriangleincrementAlphabets.js
+++ striver/Patterns/rightAngleTriangleincrementAlphabets.js
@@ -11,10 +11,13 @@
 // rightAngleTriangleincrementAlphabets(5);
 
 const rightAngleTriangleincrementAlphabets = (n) => {
+
   for (let i = 0; i <= n; i++) {
-    let row = "";
-    for (let j = 0; j <= i; j++) {
-      row += String.fromCharCode("A".charCodeAt(0) + i);
+      let row = "";
+      let num = 0;
+    for (let j = n-1; j > i; j--) {
+        row += String.fromCharCode("A".charCodeAt(0) + num);
+        num+=1
     }
     console.log(row);
   }
