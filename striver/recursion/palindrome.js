eeecf242048b938754cfe6d3d0070f75798d69f0
Priyanka Katre <priyankack1707@gmail.com>
2024-11-15 21:55:23 +0000

    palindrome using 2 pointer

--- striver/recursion/palindrome.js
+++ striver/recursion/palindrome.js
@@ -1,9 +1,29 @@
-const palindrome = (str) => {
-    str= str.toLowerCase()
-    if (str.length <= 1) return true;
-    if (str[0] === str[str.length - 1]) {
-        return palindrome(str.substring(1, str.length - 1));
+// const palindrome = (str) => {
+//     str= str.toLowerCase()
+//     if (str.length <= 1) return true;
+//     if (str[0] === str[str.length - 1]) {
+//         return palindrome(str.substring(1, str.length - 1));
+//     }
+//     return false
+// }
+
+const palindrome = (string) => {
+    let str = string.toLowerCase().split("");
+    if(str.length === 0) return
+    let left = 0;
+    let right = str.length - 1;
+
+    while (left < right) {
+        [str[left], str[right]] = [str[right], str[left]];
+        left++;
+        right--
+    };
+    if (string.toLowerCase() === str.join('')) {
+        return true
     }
-    return false
+    else {
+        return false
+    }
 }
+
 console.log(palindrome("ABCDCBa"));
