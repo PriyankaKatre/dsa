19069388a837a7937978070a6b89a1be4266e09e
Priyanka Katre <priyankack1707@gmail.com>
2024-11-19 17:21:49 +0000

    roate an array by 1 element

--- striver/Arrays/isSorted.js
+++ striver/Arrays/isSorted.js
@@ -1,9 +1,23 @@
 const isSorted = (arr) => {
     for (let i = 1; i < arr.length; i++) {
-        if (arr[i] < arr[i - 1]) {
+        if (arr[i] >= arr[i - 1]) {
+        } else {
             return false
         }
     }
-    return true;
+    return true
 }
-console.log(isSorted([1, 2, 3, 4,5, 8, 6]))
+console.log(isSorted([1, 2, 2, 3, 5, 4, 5, 6]))
+
+const removeDuplicate = (arr) => {
+    let sortedArray = arr.filter((item) => item > 0).sort((a,b) => a-b)
+    let j = 0;
+    for (let i = 1; i < sortedArray.length; i++) {
+        if (sortedArray[i] !== sortedArray[j]) {
+            j++;
+            sortedArray[j] = sortedArray[i]
+        }
+    }
+    return sortedArray.slice(0, j+1)
+}
+console.log(removeDuplicate([1, 2, 2, 3, 7, 5, 4, 5, 6, -2. -3]))
