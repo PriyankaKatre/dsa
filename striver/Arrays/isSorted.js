e829acb56d6bb7f06183fa48907ed3d953feefce
Priyanka Katre <priyankack1707@gmail.com>
2024-11-19 16:03:31 +0000

    is sorted array

---
+++ striver/Arrays/isSorted.js
@@ -0,0 +1,9 @@
+const isSorted = (arr) => {
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < arr[i - 1]) {
+            return false
+        }
+    }
+    return true;
+}
+console.log(isSorted([1, 2, 3, 4,5, 8, 6]))
