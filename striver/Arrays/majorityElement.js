b332eec74ea941d9308943d922bb50c4d5720585
Priyanka Katre <priyankack1707@gmail.com>
2024-11-26 15:31:57 +0000

    Majority Element

---
+++ striver/Arrays/majorityElement.js
@@ -0,0 +1,20 @@
+const majorityElement = (arr, n) => {
+    let map = {}
+    for (let i = 0; i < arr.length; i++) {
+        if (!map[arr[i]]) {
+            map[arr[i]] = 1
+        }
+        else {
+            map[arr[i]]++
+        }
+    }
+
+    for (let key of Object.keys(map)) {
+        if (map[key] > n / 2) {
+            return key
+        }
+    }
+    console.log(map)
+}
+
+console.log(majorityElement([3, 2, 3], 3));
