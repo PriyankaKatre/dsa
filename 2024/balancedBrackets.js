e4254faf9699505b86b2ec74ccc8b067dc741ef8
Priyanka Katre <priyankack1707@gmail.com>
2025-04-11 10:38:38 +0100

    balanced Brackets check with optimize and  recursive approach

--- 2024/balancedBrackets.js
+++ 2024/balancedBrackets.js
@@ -1,3 +1,71 @@
+let str = '[]{}()'
+
+const isValid = (str) =>{
+    if(str.length <= 1) {
+        return false
+    }
+	let pairs = {
+		'(':')',
+		'{':'}',
+		'[':']'
+	}
+	let result = [];
+	for(let i = 0 ; i<str.length; i++) {
+		if(pairs[str[i]]) {
+			result.push(str[i])
+		}
+		else if(pairs[result.pop()] !== str[i]) {
+			return false
+		}
+	}
+	return result.length === 0
+}
+
+
+console.log(isValid(str))
+
+
+//Using Recursion
+const isValid = () =>{
+	let pairs = {
+		'(':')',
+		'{':'}',
+		'[':']'		
+	}
+	let check = (str, stack=[]) => {
+      
+      if (str.length === 0) {
+            return stack.length === 0;
+        }
+
+		let char = str[0];
+		let rest = str.slice(1)
+		if(pairs[char]) {
+			stack.push(char);
+		}
+		else if(char === ')' || char ==='}' || char===']') {
+			if(stack.length === 0) {
+				return false
+			}
+			const top = stack.pop()
+			if(pairs[top] !== char) {
+				return false
+			}
+			return check(rest, stack)
+		}
+		return check(rest, stack)
+		
+	}
+	return check('')
+}
+
+console.log(isValid())
+
+
+
+
+
+
 const string = '({[{()}]})'
 
 const balancedBrackt = (string) => {
